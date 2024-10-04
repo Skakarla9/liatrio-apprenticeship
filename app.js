0722157b17cd09887338951c531fc7ba18934c6e
sanchitkakarla <skakarla@csuchico.edu>
2024-10-04 16:55:52 -0700

    Till GitHub Actions

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  const response = {
+    message: 'My name is Sanchit Kakarla',
+    timestamp: Date.now()
+  };
+  res.json(response);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
