af654d188ae1fb64c21d9f1870b746dc0434cb2c
sanchitkakarla <skakarla@csuchico.edu>
2024-10-08 13:13:36 -0700

    continue

--- app.js
+++ app.js
@@ -13,11 +13,3 @@ const port = process.env.PORT || 80;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-app.get('/', (req, res) => {
-  const response = {
-    message: 'My name is Sanchit Kakarla',
-    timestamp: Date.now()
-  };
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(response));  // This ensures it's minified
-});
